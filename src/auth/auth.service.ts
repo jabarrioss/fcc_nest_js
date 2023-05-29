d271371489d2858fb770b6a6156de05ef4e8ef7e
Jhonny Barrios <jhonnybarrios@lavenir.com.co>
2023-05-29 01:30:08 -0400

    Add AuthDto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService){
+        
+    }
     signUp(){
         return {mgs:"I am signup"};
     }
